b0b6082d4b489ca0a4861f4bc8d1a1afe368b1ef
sanoarul123 <sanoarul123@gmail.com>
2025-05-31 14:23:36 +0600

    Created POST api using Serializer with error message

--- api/views.py
+++ api/views.py
@@ -6,9 +6,16 @@ from rest_framework.decorators import api_view
 
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentView(request):
     if request.method == 'GET':
         students = student.objects.all()
         serializer = StudentSerializer(students,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
